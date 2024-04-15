5bbc59f128d667c42e7c428e88f1a6816498b056
Steven C <lemonkyo2222@outlook.com>
2024-04-15 17:16:32 +0800

    Style: Fix

--- server/src/GameServer/Connection/EventArgs/AfterMessageReceiveEventArgs.cs
+++ server/src/GameServer/Connection/EventArgs/AfterMessageReceiveEventArgs.cs
@@ -6,7 +6,7 @@ public class AfterMessageReceiveEventArgs : EventArgs
     /// The message received
     /// </summary>
     public Message Message { get; }
-    public Guid SocketId { get;}
+    public Guid SocketId { get; }
 
     /// <summary>
     /// Constructor
