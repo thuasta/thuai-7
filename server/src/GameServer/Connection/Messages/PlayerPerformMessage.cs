0c8cae17d4802f6007da9328e5e3580053797bb9
Steven C <lemonkyo2222@outlook.com>
2024-04-22 21:25:32 +0800

    Refactor: Inherit player actions from PerformMessage

--- server/src/GameServer/Connection/Messages/PlayerPerformMessage.cs
+++ server/src/GameServer/Connection/Messages/PlayerPerformMessage.cs
@@ -2,7 +2,14 @@ using System.Text.Json.Serialization;
 
 namespace GameServer.Connection;
 
-public record PerformAbandonMessage : Message
+public abstract record PerformMessage : Message
+{
+
+    [JsonPropertyName("token")]
+    public string Token { get; init; } = "";
+}
+
+public record PerformAbandonMessage : PerformMessage
 {
     [JsonPropertyName("messageType")]
     public override string MessageType { get; init; } = "PERFORM_ABANDON";
@@ -10,20 +17,14 @@ public record PerformAbandonMessage : Message
     [JsonPropertyName("numb")]
     public int Numb { get; init; }
 
-    [JsonPropertyName("token")]
-    public string Token { get; init; } = "";
-
     [JsonPropertyName("targetSupply")]
     public string TargetSupply { get; init; } = "";
 }
 
-public record PerformPickUpMessage : Message
+public record PerformPickUpMessage : PerformMessage
 {
     [JsonPropertyName("messageType")]
     public override string MessageType { get; init; } = "PERFORM_PICK_UP";
-
-    [JsonPropertyName("token")]
-    public string Token { get; init; } = "";
 
     [JsonPropertyName("num")]
     public int Num { get; init; }
@@ -44,37 +45,28 @@ public record PerformPickUpMessage : Message
     }
 }
 
-public record PerformSwitchArmMessage : Message
+public record PerformSwitchArmMessage : PerformMessage
 {
     [JsonPropertyName("messageType")]
     public override string MessageType { get; init; } = "PERFORM_SWITCH_ARM";
-
-    [JsonPropertyName("token")]
-    public string Token { get; init; } = "";
 
     [JsonPropertyName("targetFirearm")]
     public string TargetFirearm { get; init; } = "";
 }
 
-public record PerformUseMedicineMessage : Message
+public record PerformUseMedicineMessage : PerformMessage
 {
     [JsonPropertyName("messageType")]
     public override string MessageType { get; init; } = "PERFORM_USE_MEDICINE";
-
-    [JsonPropertyName("token")]
-    public string Token { get; init; } = "";
 
     [JsonPropertyName("medicineName")]
     public string MedicineName { get; init; } = "";
 }
 
-public record PerformUseGrenadeMessage : Message
+public record PerformUseGrenadeMessage : PerformMessage
 {
     [JsonPropertyName("messageType")]
     public override string MessageType { get; init; } = "PERFORM_USE_GRENADE";
-
-    [JsonPropertyName("token")]
-    public string Token { get; init; } = "";
 
     [JsonPropertyName("targetPosition")]
     public TargetPosition TargetPos { get; init; } = new();
@@ -89,13 +81,10 @@ public record PerformUseGrenadeMessage : Message
     }
 }
 
-public record PerformMoveMessage : Message
+public record PerformMoveMessage : PerformMessage
 {
     [JsonPropertyName("messageType")]
     public override string MessageType { get; init; } = "PERFORM_MOVE";
-
-    [JsonPropertyName("token")]
-    public string Token { get; init; } = "";
 
     [JsonPropertyName("destination")]
     public DestinationInfo Destination { get; init; } = new();
@@ -110,23 +99,17 @@ public record PerformMoveMessage : Message
     }
 }
 
-public record PerformStopMessage : Message
+public record PerformStopMessage : PerformMessage
 {
     [JsonPropertyName("messageType")]
     public override string MessageType { get; init; } = "PERFORM_STOP";
 
-    [JsonPropertyName("token")]
-    public string Token { get; init; } = "";
-
 }
 
-public record PerformAttackMessage : Message
+public record PerformAttackMessage : PerformMessage
 {
     [JsonPropertyName("messageType")]
     public override string MessageType { get; init; } = "PERFORM_ATTACK";
-
-    [JsonPropertyName("token")]
-    public string Token { get; init; } = "";
 
     [JsonPropertyName("targetPosition")]
     public TargetPosition TargetPos { get; init; } = new();
@@ -141,31 +124,22 @@ public record PerformAttackMessage : Message
     }
 }
 
-public record GetPlayerInfoMessage : Message
+public record GetPlayerInfoMessage : PerformMessage
 {
     [JsonPropertyName("messageType")]
     public override string MessageType { get; init; } = "GET_PLAYER_INFO";
-
-    [JsonPropertyName("token")]
-    public string Token { get; init; } = "";
 }
 
-public record GetMapMessage : Message
+public record GetMapMessage : PerformMessage
 {
     [JsonPropertyName("messageType")]
     public override string MessageType { get; init; } = "GET_MAP";
-
-    [JsonPropertyName("token")]
-    public string Token { get; init; } = "";
 }
 
-public record ChooseOriginMessage : Message
+public record ChooseOriginMessage : PerformMessage
 {
     [JsonPropertyName("messageType")]
     public override string MessageType { get; init; } = "CHOOSE_ORIGIN";
-
-    [JsonPropertyName("token")]
-    public string Token { get; init; } = "";
 
     [JsonPropertyName("originPosition")]
     public OriginPosition OriginPos { get; init; } = new();
