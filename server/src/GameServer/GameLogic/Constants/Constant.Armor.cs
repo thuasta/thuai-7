30b0b3530b5bd01f12f30bdbdc7473b19aac615c
Andy Zijian Zhang <futrime+git@outlook.com>
2024-05-09 00:47:02 +0000

    feat: adjust armor

--- server/src/GameServer/GameLogic/Constants/Constant.Armor.cs
+++ server/src/GameServer/GameLogic/Constants/Constant.Armor.cs
@@ -3,8 +3,8 @@ namespace GameServer.GameLogic;
 public partial class Constant
 {
     public const int NO_ARMOR_DEFENSE = 0;
-    public const int PRIMARY_ARMOR_DEFENSE = 100;
+    public const int PRIMARY_ARMOR_DEFENSE = 50;
 
-    public const int PREMIUM_ARMOR_DEFENSE = 200;
+    public const int PREMIUM_ARMOR_DEFENSE = 100;
 
 }
