bfb02ffa1882aa1cf6b8b426bd03558c890b9f4b
Andy Zijian Zhang <futrime+git@outlook.com>
2024-05-09 01:01:35 +0000

    feat: boost grenade

--- server/src/GameServer/GameLogic/Constants/Constant.Grenade.cs
+++ server/src/GameServer/GameLogic/Constants/Constant.Grenade.cs
@@ -3,13 +3,13 @@ namespace GameServer.GameLogic;
 public partial class Constant
 {
     //手榴弹爆炸延时的tick数
-    public const int GRENADE_EXPLODE_TICK = 100;
+    public const int GRENADE_EXPLODE_TICK = 169;
     //手雷爆炸的伤害的线性衰减参数
-    public const double GRENADE_DAMAGE_DECAY = 13.83;
+    public const double GRENADE_DAMAGE_DECAY = 50;
     //手雷爆炸的伤害的最大值
-    public const double GRENADE_MAX_DAMAGE = 108.07;
+    public const double GRENADE_MAX_DAMAGE = 1690;
     //手雷爆炸的伤害的最大半径
-    public const double GRENADE_MAX_RADIUS = 6;
+    public const double GRENADE_MAX_RADIUS = 10;
 
     public const double GRENADE_THROW_RADIUS = 60;
 
