2c6c7df2f5abbb17880b368eec4fd6bae3a2404e
Steven C <lemonkyo2222@outlook.com>
2024-05-15 23:28:10 +0800

    Feat: Adjust range

--- server/src/GameServer/GameLogic/Constants/Constant.Weapons.cs
+++ server/src/GameServer/GameLogic/Constants/Constant.Weapons.cs
@@ -5,20 +5,20 @@ public partial class Constant
     public const float FIST_RANGE = 1;
     public const int FIST_DAMAGE = 18;
     public const int FIST_COOLDOWNTICKS = 0;
-    public const float S686_RANGE = 37;
+    public const float S686_RANGE = 7;
     public const int S686_SINGLE_BULLET_DAMAGE = 26;
     //弹头数目
     public const int S686_BULLET_NUM = 9;
     //两颗弹头角度差
-    public const int S686_DELTA_DEG = 3;
-    public const int S686_COOLDOWNTICKS = 28;
-    public const float VECTOR_RANGE = 35;
+    public const int S686_DELTA_DEG = 2;
+    public const int S686_COOLDOWNTICKS = 8;
+    public const float VECTOR_RANGE = 20;
     public const int VECTOR_DAMAGE = 31;
     public const int VECTOR_COOLDOWNTICKS = 3;
-    public const float M16_RANGE = 90;
+    public const float M16_RANGE = 40;
     public const int M16_DAMAGE = 43;
     public const int M16_COOLDOWNTICKS = 8;
-    public const float AWM_RANGE = 94.5f;
+    public const float AWM_RANGE = 75;
     public const int AWM_DAMAGE = 105;
     public const int AWM_COOLDOWNTICKS = 55;
 }
