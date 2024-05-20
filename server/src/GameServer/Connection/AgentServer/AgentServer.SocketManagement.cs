6ef8d9809d66ac0760b0c21d77ab9d44577f6483
Steven C <lemonkyo2222@outlook.com>
2024-05-20 20:53:01 +0800

    Chore: Add exception handling

--- server/src/GameServer/Connection/AgentServer/AgentServer.SocketManagement.cs
+++ server/src/GameServer/Connection/AgentServer/AgentServer.SocketManagement.cs
@@ -7,91 +7,121 @@ public partial class AgentServer
 {
     private void AddSocket(Guid socketId, IWebSocketConnection socket)
     {
-        _sockets.TryAdd(socketId, socket);
+        try
+        {
+            _sockets.TryAdd(socketId, socket);
 
-        _socketRawTextReceivingQueue.AddOrUpdate(
-            socketId,
-            new ConcurrentQueue<string>(),
-            (key, oldValue) => new ConcurrentQueue<string>()
-        );
+            _socketRawTextReceivingQueue.AddOrUpdate(
+                socketId,
+                new ConcurrentQueue<string>(),
+                (key, oldValue) => new ConcurrentQueue<string>()
+            );
 
-        _socketMessageSendingQueue.AddOrUpdate(
-            socketId,
-            new ConcurrentQueue<Message>(),
-            (key, oldValue) => new ConcurrentQueue<Message>()
-        );
+            _socketMessageSendingQueue.AddOrUpdate(
+                socketId,
+                new ConcurrentQueue<Message>(),
+                (key, oldValue) => new ConcurrentQueue<Message>()
+            );
 
-        // Cancel the previous task if it exists
-        _ctsForParsingMessage.TryGetValue(socketId, out CancellationTokenSource? ctsForParsingMessage);
-        ctsForParsingMessage?.Cancel();
-        _ctsForParsingMessage.TryRemove(socketId, out _);
+            // Cancel the previous task if it exists
+            _ctsForParsingMessage.TryGetValue(socketId, out CancellationTokenSource? ctsForParsingMessage);
+            ctsForParsingMessage?.Cancel();
+            _ctsForParsingMessage.TryRemove(socketId, out _);
 
-        _ctsForSendingMessage.TryGetValue(socketId, out CancellationTokenSource? ctsForSendingMessage);
-        ctsForSendingMessage?.Cancel();
-        _ctsForSendingMessage.TryRemove(socketId, out _);
+            _ctsForSendingMessage.TryGetValue(socketId, out CancellationTokenSource? ctsForSendingMessage);
+            ctsForSendingMessage?.Cancel();
+            _ctsForSendingMessage.TryRemove(socketId, out _);
 
-        // Create new tasks for parsing and sending messages
-        Task parsingTask = CreateTaskForParsingMessage(socketId);
-        parsingTask.Start();
+            // Create new tasks for parsing and sending messages
+            Task parsingTask = CreateTaskForParsingMessage(socketId);
+            parsingTask.Start();
 
-        _tasksForParsingMessage.AddOrUpdate(
-            socket.ConnectionInfo.Id,
-            parsingTask,
-            (key, oldValue) =>
-            {
-                oldValue?.Dispose();
-                return parsingTask;
-            }
-        );
+            _tasksForParsingMessage.AddOrUpdate(
+                socket.ConnectionInfo.Id,
+                parsingTask,
+                (key, oldValue) =>
+                {
+                    oldValue?.Dispose();
+                    return parsingTask;
+                }
+            );
 
-        Task sendingTask = CreateTaskForSendingMessage(socketId);
-        sendingTask.Start();
+            Task sendingTask = CreateTaskForSendingMessage(socketId);
+            sendingTask.Start();
 
-        _tasksForSendingMessage.AddOrUpdate(
-            socketId,
-            sendingTask,
-            (key, oldValue) =>
-            {
-                oldValue?.Dispose();
-                return sendingTask;
-            }
-        );
+            _tasksForSendingMessage.AddOrUpdate(
+                socketId,
+                sendingTask,
+                (key, oldValue) =>
+                {
+                    oldValue?.Dispose();
+                    return sendingTask;
+                }
+            );
+        }
+        catch (Exception ex)
+        {
+            _logger.Error($"Failed to add {GetAddress(socket)}: {ex.Message}");
+            _logger.Debug($"{ex}");
+        }
     }
 
     private void RemoveSocket(Guid socketId)
     {
-        _sockets.TryRemove(socketId, out _);
-        _socketTokens.TryRemove(socketId, out _);
+        try
+        {
+            _sockets.TryRemove(socketId, out _);
+            _socketTokens.TryRemove(socketId, out _);
 
-        _ctsForParsingMessage.TryGetValue(socketId, out CancellationTokenSource? ctsForParsingMessage);
-        ctsForParsingMessage?.Cancel();
-        _ctsForParsingMessage.TryRemove(socketId, out _);
+            _ctsForParsingMessage.TryGetValue(socketId, out CancellationTokenSource? ctsForParsingMessage);
+            ctsForParsingMessage?.Cancel();
+            _ctsForParsingMessage.TryRemove(socketId, out _);
 
-        _ctsForSendingMessage.TryGetValue(socketId, out CancellationTokenSource? ctsForSendingMessage);
-        ctsForSendingMessage?.Cancel();
-        _ctsForSendingMessage.TryRemove(socketId, out _);
+            _ctsForSendingMessage.TryGetValue(socketId, out CancellationTokenSource? ctsForSendingMessage);
+            ctsForSendingMessage?.Cancel();
+            _ctsForSendingMessage.TryRemove(socketId, out _);
 
-        _tasksForParsingMessage.TryRemove(socketId, out _);
-        _tasksForSendingMessage.TryRemove(socketId, out _);
+            _tasksForParsingMessage.TryRemove(socketId, out _);
+            _tasksForSendingMessage.TryRemove(socketId, out _);
 
-        _socketMessageSendingQueue.TryRemove(socketId, out _);
-        _socketRawTextReceivingQueue.TryRemove(socketId, out _);
+            _socketMessageSendingQueue.TryRemove(socketId, out _);
+            _socketRawTextReceivingQueue.TryRemove(socketId, out _);
+        }
+        catch (Exception ex)
+        {
+            _logger.Error($"Failed to remove {GetAddress(socketId)}: {ex.Message}");
+            _logger.Debug($"{ex}");
+        }
     }
 
     private string GetAddress(IWebSocketConnection socket)
     {
-        return $"{socket.ConnectionInfo.ClientIpAddress}: {socket.ConnectionInfo.ClientPort}";
+        try
+        {
+            return $"{socket.ConnectionInfo.ClientIpAddress}: {socket.ConnectionInfo.ClientPort}";
+        }
+        catch (Exception)
+        {
+            return "[UNKNOWN]";
+        }
     }
 
     private string GetAddress(Guid socketId)
     {
-        if (_sockets.TryGetValue(socketId, out IWebSocketConnection? socket) && socket is not null)
+        try
         {
-            return GetAddress(socket);
+            if (_sockets.TryGetValue(socketId, out IWebSocketConnection? socket) && socket is not null)
+            {
+                return GetAddress(socket);
+            }
+            else
+            {
+                return "[UNKNOWN]";
+            }
         }
-        else
+        catch (Exception)
         {
-            return "UNKNOWN";
+            return "[UNKNOWN]";
         }
     }
 }
